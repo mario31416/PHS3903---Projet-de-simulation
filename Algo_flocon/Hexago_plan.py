f235fe24bad421abd4ce4badad65ecb73e58f34c
mario31416 <marie.lafontaine@polymtl.ca>
2023-03-17 13:37:46 -0400

    Petit bug réglé

--- Algo_flocon/Hexago_plan.py
+++ Algo_flocon/Hexago_plan.py
@@ -1,10 +1,10 @@
 from hexalattice.hexalattice import *
 import numpy as np
 
-N = 100
+N = 10
 hex_centers, _ = create_hex_grid(nx=N,
                                  ny=N,
-                                 do_plot=True)
+                                 do_plot=False)
                                  
 
 x_hex_coords = hex_centers[:, 0]
@@ -14,10 +14,11 @@ y_hex_coords = hex_centers[:, 1]
 # colors = sample_colors_from_image_by_grid(image_path, x_hex_coords, y_hex_coords)
 
 a = [[1,1,1]]
-for i in range(99) :
+for i in range(N**2-1) :
     a.append([1,1,1])
 
 colors = np.array(a)
+print('a', a)
 
 M = [25, 63, 66]
 for m in M:
