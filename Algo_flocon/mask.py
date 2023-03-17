8f3287f05c06f6c591c1c6d8c2bad3a78d1f1df2
Ytrem <florentin.belfio@icloud.com>
2023-03-17 16:29:17 -0400

    mask version glace qui marche

--- Algo_flocon/mask.py
+++ Algo_flocon/mask.py
@@ -24,27 +24,50 @@ colors = color_ice + color_vapor + color_quasi
 
 mask_tot = np.full((100,4), [0, 0, 0, rho])    # Mask totale a=(0 ou 1 si dans cristal) b : boundary mass (quasi-liquid)
                                     # c : cristal mass (ice) d : diffusive mass (vapor)
-mask_tot[int((len(mask_tot) / 2) + N / 2)] = [1, 0, 0, 0]
-print(mask_tot)
+mask_tot[int((len(mask_tot) / 2) + N / 2)] = [1, 0, 1, 0]
 
-final_ice = []
+ice = mask_tot[:,2]
+final_mask_ice = []
 for i in range(len(mask_tot)):
     for j in range(3):
-        final_ice.append(mask_tot[i])
+        final_mask_ice.append(ice[i])
 
 
-#colors = np.full((100, 3), [0, 0, 1])
-#final_ice_reshaped = np.reshape(final_ice, (N**2, 3))
-#final_color = (colors + final_ice_reshaped) / np.max(colors + final_ice_reshaped)
+final_mask_ice_reshaped = np.reshape(final_mask_ice, (N**2, 3))
+final_color_ice = (color_ice * final_mask_ice_reshaped) 
 
 colors = color_ice + color_vapor + color_quasi
 
+
+plt.figure(1)
+plt.title('Mask total')
 plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       # Plot total des 3 phases 
                                       face_color= colors,
                                       edge_color=colors,
                                       min_diam=1,
                                       plotting_gap=0.0,
                                       rotate_deg=0)
+plt.figure(2)
+plt.title('Mask glace')
+plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       
+                                      face_color= final_color_ice,
+                                      edge_color=colors,
+                                      min_diam=1,
+                                      plotting_gap=0.0,
+                                      rotate_deg=0)
+
+plt.figure(3)
+plt.title('Mask vapeur')
+plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       
+                                      face_color= color_vapor,
+                                      edge_color=colors,
+                                      min_diam=1,
+                                      plotting_gap=0.0,
+                                      rotate_deg=0)
+
+
+
+
 
 
 plt.show()
