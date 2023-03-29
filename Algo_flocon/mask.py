ec2bf6f19e266956ed51ba032a63a94bc2d150a0
Ytrem <florentin.belfio@icloud.com>
2023-03-29 12:02:48 -0400

    update du plot avec tuto

--- Algo_flocon/mask.py
+++ Algo_flocon/mask.py
@@ -1,6 +1,7 @@
 from hexalattice.hexalattice import *
 import numpy as np
 from fonctions_frontieres import *
+from fonctions_plot import *
 
 # ---------PARAMETRES--------------------
 
@@ -83,85 +84,18 @@ for t in range(15):
 
 # ---------------------PLOT RESULTS------------------------------------
 
+""""
+TUTO PLOT :    
+    - Pour  plot tout : plot_total(mask_tot, color_ice, color_vapor, color_quasi, x_hex_coords, y_hex_coords, N)
 
-# GLACE 
-ice = mask_tot[:,2]
-final_mask_ice = []
-for i in range(len(mask_tot)):
-    for j in range(3):
-        final_mask_ice.append(ice[i])
+    POUR UNE SEULE PHASE AJOUTER plt.show()
 
-final_mask_ice_reshaped = np.reshape(final_mask_ice, (N**2, 3))
-final_color_ice = (color_ice * final_mask_ice_reshaped) 
-# VAPEUR
-vapor = mask_tot[:,3]
-final_mask_vapor = []
-for i in range(len(mask_tot)):
-    for j in range(3):
-        final_mask_vapor.append(vapor[i])
-
-final_mask_vapor_reshaped = np.reshape(final_mask_vapor, (N**2, 3))
-final_color_vapor = (color_vapor * final_mask_vapor_reshaped) 
-
-
-# QUASI-LIQUIDE
-quasi_liquid = mask_tot[:,1]
-final_mask_quasi_liquid = []
-for i in range(len(mask_tot)):
-    for j in range(3):
-        final_mask_quasi_liquid.append(quasi_liquid[i])
-
-final_mask_quasi_liquid_reshaped = np.reshape(final_mask_quasi_liquid, (N**2, 3))
-final_color_quasi_liquid = (color_quasi * final_mask_quasi_liquid_reshaped) 
+    - Pour plot glace : plot_ice(mask_tot, color_ice, x_hex_coords, y_hex_coords, N)
+    - Pour plot vapeur : plot_vapeur(mask_tot, color_vapor, x_hex_coords, y_hex_coords, N)
+    - pour plot QLL : plot_quasi(mask_tot, color_quasi, x_hex_coords, y_hex_coords, N)
+"""
+plot_total(mask_tot, color_ice, color_vapor, color_quasi, x_hex_coords, y_hex_coords, N)
 
 
 
-final_colors = final_color_ice + final_color_vapor + final_color_quasi_liquid
-
-# PLOT
-
-plt.figure(1)
-plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       # Plot total des 3 phases 
-                                      face_color= final_colors,
-                                      edge_color=final_colors,
-                                      min_diam=1,
-                                      plotting_gap=0.0,
-                                      rotate_deg=0)
-plt.title('Mask total')
-plt.figure(2)
-
-plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       
-                                      face_color= final_color_ice,
-                                      edge_color=final_color_ice,
-                                      min_diam=1,
-                                      plotting_gap=0.0,
-                                      rotate_deg=0)
-plt.title('Mask glace')
-
-plt.figure(3)
-
-plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       
-                                      face_color=final_color_vapor,
-                                      edge_color=final_color_vapor,
-                                      min_diam=1,
-                                      plotting_gap=0.0,
-                                      rotate_deg=0)
-
-plt.title('Mask vapeur')
-
-plt.figure(4)
-
-plot_single_lattice_custom_colors(x_hex_coords, y_hex_coords,       
-                                      face_color=final_color_quasi_liquid,
-                                      edge_color=final_color_quasi_liquid,
-                                      min_diam=1,
-                                      plotting_gap=0.0,
-                                      rotate_deg=0)
-
-plt.title('Mask quasi liquid')
-
-
-
-
-plt.show()
 
